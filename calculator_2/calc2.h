0801a248c571f7b0a103ac2ee99cc615ed3b3442
Benjamin Clemente <148456305+Benji-clm@users.noreply.github.com>
2024-09-11 12:14:09 +0100

    Update calc2.h
    added comments for clarity

--- calculator_2/calc2.h
+++ calculator_2/calc2.h
@@ -1,3 +1,4 @@
+// enum is a user-defined data type, we give it names to represent the three different values of enum, namely our three node types, we also assign an alias "nodeEnum" to enum
 typedef enum
 {
     typeCon,
@@ -5,6 +6,7 @@ typedef enum
     typeOpr
 } nodeEnum;
 
+// We proceed to define structure names for each of the three node types
     /* constants */
 typedef struct
 {
@@ -17,6 +19,10 @@ typedef struct
     int i; /* subscript to sym array */
 } idNodeType;
 
+// The operator nodetype is special, in that it hols:
+// - the operator as an integer code
+// - the number of *operands* the operator holds (i.e. a + b -> + operator takes two operands)
+// - An array of pointers to the operands.
     /* operators */
 typedef struct
 {
@@ -25,6 +31,8 @@ typedef struct
     struct nodeTypeTag **op; /* operands */
 } oprNodeType;
 
+// Finally, and most importantly, we define the primary structure (the core data type), to represent any of the three node types in our AST.
+// We declare a nodeEnum, with identifier "type", and this can take the value of only one of the three NodeType. We also give this structure the name "nodeType"
 typedef struct nodeTypeTag
 {
     nodeEnum type; /* type of node */
@@ -36,4 +44,5 @@ typedef struct nodeTypeTag
     };
 } nodeType;
 
+// This is the symbol table which holds the values for the 26, a-z, possible variables.
 extern int sym[26];
